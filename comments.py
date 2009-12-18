659b36800fb8e895054ed3cda87eda6069f1b2f1
Thomas Habets <thomas@habets.pp.se>
2009-12-18 22:40:59 +0100

    initial commit

---
+++ comments.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import json
+import httplib
+import numpy
+import sys
+
+def GET(path): 
+    conn = httplib.HTTPConnection("www.reddit.com")
+    conn.request("GET", path)
+    r1 = conn.getresponse()
+    if r1.status != 200:
+        print r1.status
+        raise "barn"
+    ret = r1.read()
+    conn.close()
+    return ret
+
+def dump(user, count, after):
+    s = "/user/%s/comments" % (user)
+    a = ""
+    if after:
+        a = "?after=" + after
+    path = '%s/.json%s' % (s, a)
+    print path
+    a = json.read(GET(path))
+    return a['data']['after'], [x['data'] for x in a['data']['children']]
+
+def printComment(c):
+    if not c.has_key('body'):
+        return
+    print '-------------'
+    print 'Article: %s' % (c['link_title'][:40])
+    print 'Id:      %s' % (c['id'])
+    print 'Karma:   %d (%d up / %d down)' % (c['ups'] - c['downs'],
+                                             c['ups'],
+                                             c['downs'])
+    print c['body']
+
+def printData(data):
+    data.sort(lambda x,y: (y['ups'] - y['downs']) - (x['ups'] - x['downs']))
+    for c in data[:10]:
+        printComment(c)
+
+def getData(user):
+    after = None
+    arr = []
+    n = 0
+    while True:
+        n = n+1
+        after, t = dump(user, n * 25, after)
+        arr.extend(t)
+        if after is None:
+            break
+    for e in arr:
+        for k in e.keys():
+            if isinstance(e[k], unicode):
+                e[k] = e[k].encode('utf-8')
+    return arr
+
+def main():
+    try:
+        data = json.read(open("comments-%s.json" % (sys.argv[1])).read())
+    except:
+        data = getData(sys.argv[1])
+        open("comments-%s.json" % (sys.argv[1]), "w").write(json.write(data))
+    printData(data)
+
+if __name__ == '__main__':
+    main()
---
+++ story_updown.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import json
+import httplib
+import numpy
+import sys
+
+def GET(path): 
+    conn = httplib.HTTPConnection("www.reddit.com")
+    conn.request("GET", path)
+    r1 = conn.getresponse()
+    if r1.status != 200:
+        raise "barn"
+    ret = r1.read()
+    conn.close()
+    return ret
+
+def dump(sub, count, after):
+    if sub == "":
+        s = ""
+    else:
+        s = "/r/" + sub
+    c = ""
+    a = ""
+    if count:
+        c = "?count=" + str(count)
+    if after:
+        a = "&after=" + after
+    path = '%s/.json%s%s' % (s, c, a)
+    print path
+    a = json.read(GET(path))
+    ra = []
+    for e in a['data']['children']:
+        data = e['data']
+        ra.append(
+            (data['subreddit'],
+             100 * (data['ups'] / (float(data['ups']) +data['downs']))))
+    return a['data']['after'], ra
+    
+def main():
+    sub = sys.argv[1]
+    after = None
+    arr = []
+    for n in range(10):
+        after, t = dump(sub, n * 25, after)
+        arr.extend(t)
+    v = [x[1] for x in arr]
+    print "%.2f %.2f" % (numpy.average(v), numpy.std(v))
+
+if __name__ == '__main__':
+    main()
