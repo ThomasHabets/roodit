537565049d9e91e7bcc6fffa39988c49daf957e2
Thomas Habets <thomas@habets.pp.se>
2009-12-21 15:39:12 +0100

    Generate comment links

--- comments.py
+++ comments.py
@@ -4,6 +4,7 @@ import json
 import httplib
 import numpy
 import sys
+import re
 
 def GET(path): 
     conn = httplib.HTTPConnection("www.reddit.com")
@@ -22,14 +23,18 @@ def dump(user, count, after):
     if after:
         a = "?after=" + after
     path = '%s/.json%s' % (s, a)
-    print path
     a = json.read(GET(path))
+    print path
     return a['data']['after'], [x['data'] for x in a['data']['children']]
 
 def printComment(c):
+    def noPrefix(s):
+        return re.sub('(.*_)?(.*)','\\2',s)
     if not c.has_key('body'):
         return
     print '-------------'
+    print ("http://www.reddit.com/comments/%s/dummy/%s"
+           % (noPrefix(c['link_id']), noPrefix(c['name'])))
     print 'Article: %s' % (c['link_title'][:40])
     print 'Id:      %s' % (c['id'])
     print 'Karma:   %d (%d up / %d down)' % (c['ups'] - c['downs'],
